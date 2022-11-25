f3500e92adc7dd2aea217e7a8bafba3f497b61c5
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2022-11-25 20:46:25 +0300

    Update task6.py

--- Лабораторные работы 3/task6.py
+++ Лабораторные работы 3/task6.py
@@ -2,17 +2,17 @@ list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38
 
 # TODO Оформить решение
 max_index = 0 # начнем в первого элемента
-current_num = 0
+current_num = 0  # пусть изначально тикущее и максимальное равно 0
 max_number = 0
 
-for i in range(len(list_numbers)): # находим максимально последнее число
+for i in range(len(list_numbers)): # перебераем индексы
     max_number = list_numbers[max_index]
     current_num = list_numbers[i]
-    if current_num >= max_number:
-        max_index = i
+    if current_num >= max_number:  # если текущее число больше или равно тому, которое встречали ранее
+        max_index = i  # то перезаписываем индекс максимального числа
 
 
-list_numbers[max_index] = current_num  # заменяем в списке максимальное посдленее число последним числом в списке
+list_numbers[max_index] = current_num  # заменяем в списке максимальное последенее число последним числом в списке
 list_numbers[len(list_numbers)-1] = max_number # заменяем последнее число максимальным последним
 
 
