a3dd788898ff8e163a104acfbef176214cb920cf
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2022-11-26 12:42:41 +0300

    Update task6.py

--- Лабораторные работы 3/task6.py
+++ Лабораторные работы 3/task6.py
@@ -1,20 +1,17 @@
 list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO Оформить решение
-max_index = 0 # начнем в первого элемента
-current_num = 0  # пусть изначально тикущее и максимальное равно 0
+max_index = 0   # начнем в первого элемента
+current_num = 0
 max_number = 0
 
-for i in range(len(list_numbers)): # перебераем индексы
+for i, current_num in enumerate(list_numbers):  # перебераем все индексы
     max_number = list_numbers[max_index]
-    current_num = list_numbers[i]
-    if current_num >= max_number:  # если текущее число больше или равно тому, которое встречали ранее
-        max_index = i  # то перезаписываем индекс максимального числа
+
+    if current_num >= max_number:
+        max_index = i
 
 
-list_numbers[max_index] = current_num  # заменяем в списке максимальное последенее число последним числом в списке
-list_numbers[len(list_numbers)-1] = max_number # заменяем последнее число максимальным последним
-
+list_numbers[max_index], list_numbers[len(list_numbers)-1] = current_num, max_number   #
 
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
