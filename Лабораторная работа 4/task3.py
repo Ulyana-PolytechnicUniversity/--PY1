c512c1efc8068202a4c7274795ebe2f66a7409a3
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2022-12-02 23:29:18 +0300

    Update task3.py

--- Лабораторная работа 4/task3.py
+++ Лабораторная работа 4/task3.py
@@ -1,13 +1,17 @@
 def delete(list_, index=None):
      if index is None:
-          list_1 = list_[:-1]
+          list_ = list_[:-1]
 
      else:
-          list_1 = list_[:index]
-          list_2 = list_[index + 1:]
-          list_1.extend(list_2)
+          #list_ = list_[:index] + list_[index + 1:] # Способ 1. данный способ не работает с отрицательным индексом (не использую его)
+          
+          list_1 = list_.pop(index) # Способ 2. работает с отрицательным индексом. Использую его
+          
+          #del list_[index] # Способ 3. работает с отрицательным индексом (не использую его)
+          
+          # я выбрала способ 2 (перед ним нет знака "#")
 
-     return list_1
+     return list_
 
 
 print(delete([0, 1, 2], index=0))  # [1, 2]
