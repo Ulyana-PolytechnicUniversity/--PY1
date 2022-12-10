1d8e27f37ae442c3fdc2645a7b0242c9d4ffc485
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2022-12-11 01:18:32 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -1,12 +1,14 @@
 from random import randint
-def get_unique_list_numbers() -> list[int]:
-    while len(numbers) < 16:
-        num = randint(-10, 10)
+def get_unique_list_numbers(a, b, n) -> list[int]:
+#a-нижний предел дапазона b-верхний предел диапазона n-количество чисел
+    numbers = []
+
+    while len(numbers) < n:
+        num = randint(a, b)
         if num not in numbers:
             numbers.append(num)
     return numbers
 
-
-list_unique_numbers = get_unique_list_numbers()
+list_unique_numbers = get_unique_list_numbers(-10, 10, 15)
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
