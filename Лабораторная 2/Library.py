ba90f92214ab8e1fe95b9249be64ed86f66b8d53
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2023-01-30 23:38:02 +0300

    Update Library.py

--- Лабораторная 2/Library.py
+++ Лабораторная 2/Library.py
@@ -1,11 +1,11 @@
 BOOKS_DATABASE = [
     {
-        "id_": 1,
+        "id": 1,
         "name": "test_name_1",
         "pages": 200,
     },
     {
-        "id_": 2,
+        "id": 2,
         "name": "test_name_2",
         "pages": 400,
     }
@@ -24,7 +24,7 @@ class Book:
         if id_ <= 0:
             raise ValueError("id должен быть положительным числом")
 
-        self.id_ = id_
+        self.id = id_
 
         if not isinstance(name, str):
             raise TypeError("name должно быть типа int")
@@ -40,7 +40,7 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self) -> str:
-        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
+        return f'Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})'
 
 
 # TODO написать класс Library
@@ -49,36 +49,41 @@ class Library:
     """ Класс, описывающий объект Библиотека """
     
     def __init__(self, books=[]):
+
         self.books = books
 
-    def get_next_book_id(self):
+    def get_next_book_id(self) -> int:
         if self.books == []:
             return 1
         else:
-            return self.books[-1].id_ + 1
-            
+            return self.books[-1].id + 1
 
-    def get_index_by_book_id(self, id: int):
-        if not isinstance(id, int):
+    def get_index_by_book_id(self, id_new: int) -> int:
+        if not isinstance(id_new, int):
             raise TypeError("id должен быть типа int")
-        if id <= 0:
+        if id_new <= 0:
             raise ValueError("id должен быть положительным числом")
-        self.id = id
-        
-        for ind, el in enumerate(self.books):
-            index = None
-            if el.id_ == id:
-                return ind
+        self.id_n = id_new
 
-        if index == None:
-            raise ValueError("Книги с запрашиваемым id не существует")
+        for ind, el in enumerate(self.books):
+
+            if el.id == id_new:
+                return ind
+            elif ind < self.books[-1].id - 1:
+
+                ind += 1
+            else:
+                raise ValueError("Книги с запрашиваемым id не существует")
+
+
+
 
 if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
     list_books = [
-        Book(id_=book_dict["id_"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
