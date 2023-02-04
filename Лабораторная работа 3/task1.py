fe727a8d772e5545190b021e3c55c64400b2e952
popova.us <118777367+popova-ulyana@users.noreply.github.com>
2023-02-04 15:10:29 +0300

    Update task1.py

--- Лабораторная работа 3/task1.py
+++ Лабораторная работа 3/task1.py
@@ -5,20 +5,12 @@ class Book:
         self._author = author
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
-    @name.setter
-    def name(self, name):
-        self._name = name
-
     @property
-    def author(self):
+    def author(self) -> str:
         return self._author
-
-    @author.setter
-    def author(self, author):
-        self._author = author
 
 
     def __str__(self) -> str:
@@ -31,51 +23,49 @@ class Book:
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        self.pages(pages)
+        self.pages = pages
 
     @property
     def pages(self):
-        return self.pages
+        return self._pages
 
     @pages.setter
-    def name(self, pages):
+    def pages(self, new_pages):
 
-        if not isinstance(pages, int):
+        if not isinstance(new_pages, int):
             raise TypeError("Количество страниц должно быть типа int")
-        if pages <= 0:
+        if new_pages <= 0:
             raise ValueError("Количество страниц должно быть положительным числом")
-        self.pages = pages
+        self._pages = new_pages
 
-    def __str__(self) -> str: #перегружаем str, так как добавляется аргумент pages
+    def __str__(self) -> str: #хочу видеть все атрибуты, поэтому перегружаем str, так как добавляется аргумент pages
         return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
 
-    def __repr__(self) -> str: #перегружаем repr, так как добавляется аргумент pages
+    def __repr__(self) -> str: #хочу видеть все атрибуты, поэтому перегружаем repr, так как добавляется аргумент pages
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages = {self.pages!r})"
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        self.duration(duration)
+        self.duration = duration
 
-    def __str__(self) -> str: #перегружаем str, так как добавляется аргумент duration
+    def __str__(self) -> str: #хочу видеть все атрибуты, поэтому перегружаем str, так как добавляется аргумент duration
         return f"Книга {self.name}. Автор {self.author}. Длительность {self.duration}"
 
-    def __repr__(self) -> str: #перегружаем repr, так как добавляется аргумент duration
+    def __repr__(self) -> str: #хочу видеть все атрибуты, поэтому перегружаем repr, так как добавляется аргумент duration
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration ={self.duration!r})"
 
     @property
     def duration(self):
-        return self.duration
+        return self._duration
 
     @duration.setter
-    def duration(self, duration):
+    def duration(self, new_duration):
 
-        if not isinstance(duration, int):
+        if not isinstance(new_duration, int):
             raise TypeError("Длительность должна быть типа int")
-        if duration <= 0:
+        if new_duration <= 0:
             raise ValueError("Длительность должна быть положительным числом")
-        self.pages = duration
-
-
-
+        self._duration = new_duration
+        
